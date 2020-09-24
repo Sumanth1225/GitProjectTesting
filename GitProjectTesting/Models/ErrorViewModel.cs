497140915acc0cc034eaf818c55b37fe0bc36ae4
Sumanth S R <Sumanth S R@SUMANTH-PC>
2020-09-24 18:45:14 +0530

    Test Class Added

---
+++ GitProjectTesting/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace GitProjectTesting.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
